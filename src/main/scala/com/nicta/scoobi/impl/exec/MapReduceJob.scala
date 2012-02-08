blever/scoobi

commit a6a9c80bf51c6a2a90a5f7247a53b3b03241bbff

Assign reducer tasks to output specific channels.
Each reducer tasks must only be responsible for key-values associated
with a single output channel. This is to ensure that the results of each
output channel are written to isolated sets of output files. To ensure
this make two changes:

1. Modify the calculation of the job's reducer task count to be at a
minimum the number of output channels in a given MSCR;

2. Specify the same Partitioner class for each job (rather than
generating one) that partitions solely by the key-value's tag.

src/main/scala/com/nicta/scoobi/impl/exec/MapReduceJob.scala
@@ -30,6 +30,7 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat
 import org.apache.hadoop.io.compress.GzipCodec
 import org.apache.hadoop.io.compress.CompressionCodec
 import scala.collection.mutable.{Map => MMap}
+import scala.math._
 
 import com.nicta.scoobi.Scoobi
 import com.nicta.scoobi.WireFormat
@@ -118,16 +119,14 @@ class MapReduceJob {
     val id = UniqueId.get
     val tkRtClass = TaggedKey("TK" + id, keyTypes.toMap)
     val tvRtClass = TaggedValue("TV" + id, valueTypes.toMap)
-    val tpRtClass = TaggedPartitioner("TP" + id, keyTypes.size)
 
 
     jar.addRuntimeClass(tkRtClass)
     jar.addRuntimeClass(tvRtClass)
-    jar.addRuntimeClass(tpRtClass)
 
     job.setMapOutputKeyClass(tkRtClass.clazz)
     job.setMapOutputValueClass(tvRtClass.clazz)
-    job.setPartitionerClass(tpRtClass.clazz.asInstanceOf[Class[_ <: Partitioner[_,_]]])
+    job.setPartitionerClass(classOf[TaggedPartitioner])
 
     /* Compress mapper outputs */
     job.getConfiguration.setBoolean("mapred.compress.map.output", true)
@@ -186,12 +185,14 @@ class MapReduceJob {
 
     /* Calculate the number of reducers to use with a simple heuristic:
      *
-     * Base the amount of parallelism required in the reduce phase on the size of the data output. Further,
+     * At a minimum, have at least as many reduce tasks as there are output channels. Then, base the
+     * amount of parallelism required in the reduce phase on the size of the data output. Further,
      * estimate the size of output data to be the size of the input data to the MapReduce job. Then, set
      * the number of reduce tasks to the number of 1GB data chunks in the estimated output. */
     val inputBytes: Long = mappers.toIterable.flatMap{case (src, _) => fs.globStatus(src.inputPath).map(_.getLen)}.sum
     val inputGigabytes: Int = (inputBytes / (1000 * 1000 * 1000)).toInt + 1
     job.setNumReduceTasks(inputGigabytes)
+    job.setNumReduceTasks(max(inputGigabytes, reducers.size))
 
 
     /* Run job then tidy-up. */

src/main/scala/com/nicta/scoobi/impl/rtt/TaggedPartitioner.scala
@@ -16,49 +16,14 @@
 package com.nicta.scoobi.impl.rtt
 
 import org.apache.hadoop.mapreduce.Partitioner
-import javassist._
 
 
 /** Custom paritioner for tagged key-values. */
-abstract class TaggedPartitioner extends Partitioner[TaggedKey, TaggedValue]
+class TaggedPartitioner extends Partitioner[TaggedKey, TaggedValue] {
 
-
-/** Companion object for dynamically constructing a subclass of TaggedPartitioner. */
-object TaggedPartitioner {
-
-  def apply(name: String, numTags: Int): RuntimeClass = {
-    val builder = new TaggedPartitionerClassBuilder(name, numTags)
-    builder.toRuntimeClass
-  }
-}
-
-
-/** Class for building TaggedPartitioner classes at runtime. */
-class TaggedPartitionerClassBuilder(name: String, numTags: Int) extends ClassBuilder {
-
-  def className = name
-
-  def extendClass: Class[_] = classOf[TaggedPartitioner]
-
-  def build = {
-
-    /* 'getPartition' - do hash paritioning on the key value that is tagged. */
-    val getPartitionCode =
-      "int tag = ((com.nicta.scoobi.impl.rtt.TaggedKey)$1).tag();" +
-      "switch(tag) {" +
-        (0 to numTags - 1).map { t =>
-          "case " + t + ": return (((com.nicta.scoobi.impl.rtt.TaggedKey)$1).get(tag).hashCode() & Integer.MAX_VALUE) % $3;"
-        }.mkString +
-        "default: return 0;" +
-      "}"
-    val getPartitionMethod = CtNewMethod.make(CtClass.intType,
-                                              "getPartition",
-                                              Array(pool.get("java.lang.Object"),
-                                                    pool.get("java.lang.Object"),
-                                                    CtClass.intType),
-                                              Array(),
-                                              "{" + getPartitionCode + "}",
-                                              ctClass)
-    ctClass.addMethod(getPartitionMethod)
-  }
+  /** Key-values are tagged with the output channel they are destined for. It is vital
+    * that a given reducer task only receieves key-values from a single channel. To ensure
+    * this, partition key-values by the key's tag. */
+  def getPartition(key: TaggedKey, value: TaggedValue, numPartitions: Int): Int =
+    (key.tag.hashCode() & Int.MaxValue) % numPartitions
 }

commit 39f8c2833788f640a34797da1aed501dee44e016

Partition based on actual key within TaggedKey.
Undo bug introduced by a6a9c80b. When partitioning, simply use the
hash of the actual key value wrapped within a TaggedKey.

In contrast to the comments made in the erroneous commit above, a single
reducer tasks is able to handle keys-values associated with multiple MSCR
output channels. The logic to separate channel outputs into their correct
destination files is handled by ChannelOutputFormat which internally
leverages MultipleOutputs.

src/main/scala/com/nicta/scoobi/impl/exec/MapReduceJob.scala
@@ -27,7 +27,6 @@ import org.apache.hadoop.mapreduce.Partitioner
 import org.apache.hadoop.mapreduce.OutputFormat
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat
 import scala.collection.mutable.{Map => MMap}
-import scala.math._
 import Option.{apply => ?}
 
 import com.nicta.scoobi.Scoobi
@@ -121,13 +120,15 @@ class MapReduceJob(stepId: Int) {
     val id = UniqueId.get
     val tkRtClass = TaggedKey("TK" + id, keyTypes.toMap)
     val tvRtClass = TaggedValue("TV" + id, valueTypes.toMap)
+    val tpRtClass = TaggedPartitioner("TP" + id, keyTypes.size)
 
     jar.addRuntimeClass(tkRtClass)
     jar.addRuntimeClass(tvRtClass)
+    jar.addRuntimeClass(tpRtClass)
 
     job.setMapOutputKeyClass(tkRtClass.clazz)
     job.setMapOutputValueClass(tvRtClass.clazz)
-    job.setPartitionerClass(classOf[TaggedPartitioner])
+    job.setPartitionerClass(tpRtClass.clazz.asInstanceOf[Class[_ <: Partitioner[_,_]]])
 
 
     /** Mappers:
@@ -192,14 +193,12 @@ class MapReduceJob(stepId: Int) {
 
     /* Calculate the number of reducers to use with a simple heuristic:
      *
-     * At a minimum, have at least as many reduce tasks as there are output channels. Then, base the
-     * amount of parallelism required in the reduce phase on the size of the data output. Further,
+     * Base the amount of parallelism required in the reduce phase on the size of the data output. Further,
      * estimate the size of output data to be the size of the input data to the MapReduce job. Then, set
      * the number of reduce tasks to the number of 1GB data chunks in the estimated output. */
     val inputBytes: Long = mappers.keys.map(_.inputSize()).sum
     val inputGigabytes: Int = (inputBytes / (1000 * 1000 * 1000)).toInt + 1
-    job.setNumReduceTasks(inputGigabytes)
-    val numReducers = max(inputGigabytes, reducers.size)
+    val numReducers: Int = inputGigabytes.toInt + 1
     job.setNumReduceTasks(numReducers)
 
 

src/main/scala/com/nicta/scoobi/impl/rtt/TaggedPartitioner.scala
@@ -16,14 +16,47 @@
 package com.nicta.scoobi.impl.rtt
 
 import org.apache.hadoop.mapreduce.Partitioner
+import javassist._
+
+/** Custom paritioner for tagged key-values. */
+abstract class TaggedPartitioner extends Partitioner[TaggedKey, TaggedValue]
+
+/** Companion object for dynamically constructing a subclass of TaggedPartitioner. */
+object TaggedPartitioner {
+
+  def apply(name: String, numTags: Int): RuntimeClass = {
+    val builder = new TaggedPartitionerClassBuilder(name, numTags)
+    builder.toRuntimeClass
+  }
+}
 
 
-/** Custom partitioner for tagged key-values. */
-class TaggedPartitioner extends Partitioner[TaggedKey, TaggedValue] {
+/** Class for building TaggedPartitioner classes at runtime. */
+class TaggedPartitionerClassBuilder(name: String, numTags: Int) extends ClassBuilder {
 
-  /** Key-values are tagged with the output channel they are destined for. It is vital
-    * that a given reducer task only receives key-values from a single channel. To ensure
-    * this, partition key-values by the key's tag. */
-  def getPartition(key: TaggedKey, value: TaggedValue, numPartitions: Int): Int =
-    (key.tag.hashCode() & Int.MaxValue) % numPartitions
+  def className = name
+
+  def extendClass: Class[_] = classOf[TaggedPartitioner]
+
+  def build = {
+
+    /* 'getPartition' - do hash paritioning on the key value that is tagged. */
+    val getPartitionCode =
+      "int tag = ((com.nicta.scoobi.impl.rtt.TaggedKey)$1).tag();" +
+      "switch(tag) {" +
+        (0 to numTags - 1).map { t =>
+          "case " + t + ": return (((com.nicta.scoobi.impl.rtt.TaggedKey)$1).get(tag).hashCode() & Integer.MAX_VALUE) % $3;"
+        }.mkString +
+        "default: return 0;" +
+      "}"
+    val getPartitionMethod = CtNewMethod.make(CtClass.intType,
+                                              "getPartition",
+                                              Array(pool.get("java.lang.Object"),
+                                                    pool.get("java.lang.Object"),
+                                                    CtClass.intType),
+                                              Array(),
+                                              "{" + getPartitionCode + "}",
+                                              ctClass)
+    ctClass.addMethod(getPartitionMethod)
+  }
 }